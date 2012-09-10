8e08cd55106ed5d2fdc30723ce068c6d1641db39
Tobias Bielohlawek <tobi@soundcloud.com>
2012-09-10 12:07:29 +0200

    cleanup

--- config.ru
+++ config.ru
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
 
-#$LOAD_PATH.unshift ::File.expand_path(::File.dirname(__FILE__) + '/lib')
+$LOAD_PATH.unshift ::File.expand_path(::File.dirname(__FILE__) + '/lib')
 require './server'
+
+$stdout.sync = true
 
 use Rack::Reloader, 0
 
-run Server.new
+run BunnyStall::Server.new
--- server.rb
+++ server.rb
@@ -1,44 +1,50 @@
 require 'nabaztag_hack_kit/server'
 require 'nabaztag_hack_kit/message/helper'
 
-require './graphite_stats'
+require 'graphite_stats'
 
-class Server < NabaztagHackKit::Server
-  include GraphiteStats
+module BunnyStall
+  class Server < NabaztagHackKit::Server
+    include GraphiteStats
 
-  def initialize
-    super #('bytecode.bin')
-  end
+    def initialize
+      super public_file("bytecode.bin")
+    end
 
-  get "/money" do
-    path = File.expand_path(File.join('../', 'Money.mp3'), __FILE__)
-    puts path
-    File.read(path)
-  end
+    get "/streams/:file.mp3" do
+      path = public_file("#{params[:file]}.mp3")
+      puts path
+      File.read(path)
+    end
 
 
-  on "button-pressed" do |data, request|
-    send_nabaztag({
-        PLAY_STREAM => "http://www.warteschlange.de:8888/money",
-    #   LED_L1   => [0],
-    #   LED_L2   => [0],
-    #   LED_L3   => [0],
-        LED_L4   => [0,0,0,0,100],
-    })
-  end
+    on "button-pressed" do |data, request|
+      send_nabaztag({
+          PLAY_STREAM => "money.mp3",
+      #   LED_L1   => [0],
+      #   LED_L2   => [0],
+      #   LED_L3   => [0],
+          LED_L4   => [0,0,0,0,100],
+      })
+    end
 
-  on "ping" do |data, request|
-    send_nabaztag begin
-     if payment(GraphiteStats::KEY, params[:p])
-        NabaztagHackKit::Message::Helper::wink.merge(
-          NabaztagHackKit::Message::Helper::circle
-        ).merge({
-          PLAY_STREAM => "http://www.warteschlange.de:8888/money.wav"
-        })
-      else
-        NabaztagHackKit::Message::Helper::stop
+    on "ping" do |data, request|
+      send_nabaztag begin
+       if payment(GraphiteStats::KEY, params[:p])
+          NabaztagHackKit::Message::Helper::wink.merge(
+            NabaztagHackKit::Message::Helper::circle
+          ).merge({
+            PLAY_STREAM => "money.mp3"
+          })
+        else
+          NabaztagHackKit::Message::Helper::stop
+        end
       end
+    end
+
+    private
+    def public_file(name)
+      File.expand_path(File.join('../public', name), __FILE__)
     end
   end
 end
-
